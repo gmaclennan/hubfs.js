0ee285022ce232e8a27a2071c91fecc54a09a151
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-03-27 18:39:43 -0700

    Switch to JS standard style

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var extend = require("xtend");
+var extend = require('xtend')
 
 /**
  * A mixin for [Octokat.js](https://github.com/philschatz/octokat.js) that
@@ -12,27 +12,27 @@ var extend = require("xtend");
  * larger files up to 100Mb.
  * @param  {Octokat.users.repo} repo A vaid repo returned from Octokat with
  * the call `octo.user.repos('user', 'reponame')`. See below for examples.
- * @return {Object}      returns and instance of hubfs with two methods
+ * @return {Object}      returns and instance of Hubfs with two methods
  * `readFile` and `writeFile`.
  * @example
- * var hubfs = require('hubfs');
- * var Octokat = require('octokat');
+ * var Hubfs = require('Hubfs')
+ * var Octokat = require('octokat')
  *
- * var octo = new Octocat({ username: "USER_NAME", password: "PASSWORD" });
+ * var octo = new Octocat({ username: "USER_NAME", password: "PASSWORD" })
  *
- * var gh = hubfs(octo.repos('owner', 'repo'));
+ * var gh = Hubfs(octo.repos('owner', 'repo'))
  */
-function hubfs(repo) {
-  if (!(this instanceof hubfs)) {
-    return new hubfs(repo);
+function Hubfs (repo) {
+  if (!(this instanceof Hubfs)) {
+    return new Hubfs(repo)
   }
   // Basic checks that we have a valid octokat repo.
   if ((typeof repo !== 'function') ||
     (typeof repo.contents !== 'function') ||
     (typeof repo.git !== 'function')) {
-    throw new Error('Need to provide an octokat repo to constructor');
+    throw new Error('Need to provide an octokat repo to constructor')
   }
-  this._repo = repo;
+  this._repo = repo
 }
 
 /**
@@ -51,19 +51,20 @@ function hubfs(repo) {
  * @param  {Function} callback
  * @example
  * gh.writeFile('message.txt', 'Hello Github', function (err) {
- *   if (err) throw err;
- *   console.log('It\'s saved!');
- * });
+ *   if (err) throw err
+ *   console.log('It\'s saved!')
+ * })
  */
-hubfs.prototype.writeFile = function writeFile(filename, data, options, callback) {
-  if (typeof filename !== 'string' || filename.length === 0)
-    throw new Error('Must provide a valid filename');
+Hubfs.prototype.writeFile = function writeFile (filename, data, options, callback) {
+  if (typeof filename !== 'string' || filename.length === 0) {
+    throw new Error('Must provide a valid filename')
+  }
   if (typeof callback !== 'function') {
     if (typeof options === 'function') {
-      callback = options;
-      options = undefined;
+      callback = options
+      options = undefined
     } else {
-      throw new Error('Need to provide callback');
+      throw new Error('Need to provide callback')
     }
   }
 
@@ -72,39 +73,40 @@ hubfs.prototype.writeFile = function writeFile(filename, data, options, callback
     message: 'Update/create ' + filename,
     branch: 'master',
     flag: 'w'
-  };
+  }
 
-  options = extend(writeDefaults, options);
+  options = extend(writeDefaults, options)
 
-  if (!(Buffer.isBuffer(data)))
-    data = new Buffer(data, options.encoding);
+  if (!(Buffer.isBuffer(data))) {
+    data = new Buffer(data, options.encoding)
+  }
 
   // Remove preceding slash on filename
-  filename.replace(/^\//, '');
+  filename.replace(/^\//, '')
 
   var params = {
     path: filename,
     message: options.message,
     branch: options.branch,
     content: data.toString('base64')
-  };
+  }
 
-  var file = this._repo.contents(filename);
-  var _this = this;
+  var file = this._repo.contents(filename)
+  var _this = this
 
   // First, just try to write the file without a sha, but if we get a 422 error,
   // try to get the sha and write an update
-  file.add(params, function(err, info) {
+  file.add(params, function (err, info) {
     if (info === false) return callback(new Error('Invalid repo'))
-    if (!err) return callback(null);
-    if (err.status !== 422 || options.flags === 'wx') return callback(err);
-    return _this._getSha.call(_this, params, function(err, sha) {
-      if (err) return callback(err);
-      params.sha = sha;
-      file.add(params, callback);
-    });
-  });
-};
+    if (!err) return callback(null)
+    if (err.status !== 422 || options.flags === 'wx') return callback(err)
+    return _this._getSha.call(_this, params, function (err, sha) {
+      if (err) return callback(err)
+      params.sha = sha
+      file.add(params, callback)
+    })
+  })
+}
 
 /**
  * Asynchronously read a file on Github.
@@ -122,89 +124,91 @@ hubfs.prototype.writeFile = function writeFile(filename, data, options, callback
  * @param  {Function} callback
  * @example
  * gh.readFile('/my_folder/my_file.txt', function (err, data) {
- *   if (err) throw err;
- *   console.log(data);
- * });
+ *   if (err) throw err
+ *   console.log(data)
+ * })
  */
-hubfs.prototype.readFile = function readFile(filename, options, callback) {
-  if (typeof filename !== 'string' || filename.length === 0)
-    throw new Error('Must provide a valid filename');
+Hubfs.prototype.readFile = function readFile (filename, options, callback) {
+  if (typeof filename !== 'string' || filename.length === 0) {
+    throw new Error('Must provide a valid filename')
+  }
   if (typeof callback !== 'function') {
     if (typeof options === 'function') {
-      callback = options;
-      options = undefined;
+      callback = options
+      options = undefined
     } else {
-      throw new Error('Need to provide callback');
+      throw new Error('Need to provide callback')
     }
   }
 
   var readDefaults = {
     encoding: null,
     ref: 'master'
-  };
+  }
 
-  options = extend(readDefaults, options);
+  options = extend(readDefaults, options)
 
   // Remove preceding slash on filename
-  filename.replace(/^\//, '');
+  filename.replace(/^\//, '')
 
   var params = {
     ref: options.ref
-  };
+  }
 
-  var file = this._repo.contents(filename);
-  var _this = this;
+  var file = this._repo.contents(filename)
+  var _this = this
 
-  file.fetch(params, function(err, data) {
-    if (!err) return encodeContents(data);
+  file.fetch(params, function (err, data) {
+    if (!err) return encodeContents(data)
     // We will get an error if trying to read a large file
     // (Github says > 1Mb, but sometimes smaller files fail too)
     // So we try to get the blob the long way round.
-    params.path = filename;
-    _this._getShaSlow.call(_this, params, function(err, sha) {
-      if (err) return callback(err);
-      _this._repo.git.blobs(sha).fetch(function(err, data) {
-        if (err) return callback(err);
-        encodeContents(data);
-      });
-    });
-  });
+    params.path = filename
+    _this._getShaSlow.call(_this, params, function (err, sha) {
+      if (err) return callback(err)
+      _this._repo.git.blobs(sha).fetch(function (err, data) {
+        if (err) return callback(err)
+        encodeContents(data)
+      })
+    })
+  })
 
-  function encodeContents(data) {
-    var content = new Buffer(data.content, data.encoding);
-    if (options.encoding !== null)
-      content = content.toString(options.encoding);
-    callback(null, content);
+  function encodeContents (data) {
+    var content = new Buffer(data.content, data.encoding)
+    if (options.encoding !== null) {
+      content = content.toString(options.encoding)
+    }
+    callback(null, content)
   }
-};
+}
 
-hubfs.prototype._getSha = function _getSha(params, cb) {
-  var _this = this;
+Hubfs.prototype._getSha = function _getSha (params, cb) {
+  var _this = this
 
-  _this._repo.contents(params.path).fetch(params, function(err, info) {
-    if (!err) return cb(null, info.sha);
+  _this._repo.contents(params.path).fetch(params, function (err, info) {
+    if (!err) return cb(null, info.sha)
     // This API only supports up to 1Mb, so let's try another way
-    _this._getShaSlow.call(_this, params, cb);
-  });
-};
+    _this._getShaSlow.call(_this, params, cb)
+  })
+}
 
-hubfs.prototype._getShaSlow = function _getShaSlow(params, cb) {
-  var repo = this._repo;
-  repo.git.refs('heads/' + (params.branch || params.ref)).fetch(function(err, ref) {
-    if (err) return cb(new Error('File not found'));
-    repo.git.commits(ref.object.sha).fetch(function(err, commit) {
-      if (err) return cb(new Error('File not found'));
-      repo.git.trees(commit.tree.sha + '?recursive=1').fetch(function(err, tree) {
-        if (err) return cb(new Error('File not found'));
+Hubfs.prototype._getShaSlow = function _getShaSlow (params, cb) {
+  var repo = this._repo
+  repo.git.refs('heads/' + (params.branch || params.ref)).fetch(function (err, ref) {
+    if (err) return cb(new Error('File not found'))
+    repo.git.commits(ref.object.sha).fetch(function (err, commit) {
+      if (err) return cb(new Error('File not found'))
+      repo.git.trees(commit.tree.sha + '?recursive=1').fetch(function (err, tree) {
+        if (err) return cb(new Error('File not found'))
         for (var i = 0; i < tree.tree.length; i++) {
           if (tree.tree[i].path === params.path) {
-            return cb(null, tree.tree[i].sha);
+            return cb(null, tree.tree[i].sha)
           }
         }
-        cb(new Error('File not found'));
-      });
-    });
-  });
-};
+        cb(new Error('File not found'))
+      })
+    })
+  })
+}
 
-module.exports = hubfs;
+module.exports = Hubfs
--- test.js
+++ test.js
@@ -1,107 +1,107 @@
-var hubfs = require('./');
-var test = require('tape');
-var Octokat = require('octokat');
-var dotenv = require('dotenv');
-var bufferEqual = require('buffer-equal');
+var hubfs = require('./')
+var test = require('tape')
+var Octokat = require('octokat')
+var dotenv = require('dotenv')
+var bufferEqual = require('buffer-equal')
 
-dotenv.load();
+dotenv.load()
 
-var tempRepoName = 'test' + Date.now();
-var testUser = process.env.GITHUB_USER;
+var tempRepoName = 'test' + Date.now()
+var testUser = process.env.GITHUB_USER
 
 var octo = new Octokat({
   token: process.env.GITHUB_TOKEN
-});
+})
 
-var repo = octo.repos(testUser, tempRepoName);
+var repo = octo.repos(testUser, tempRepoName)
 
-var fs = hubfs(repo);
+var fs = hubfs(repo)
 
-function setup() {
-    test('Create temporary test repo', function(t) {
-        octo.user.repos.create({ name: tempRepoName, auto_init: true }, t.end);
-    });
+function setup () {
+  test('Create temporary test repo', function (t) {
+    octo.user.repos.create({ name: tempRepoName, auto_init: true }, t.end)
+  })
 }
 
-function teardown() {
-    test('Delete temporary test repo', function(t) {
-        octo.repos(testUser, tempRepoName).remove(t.end);
-    });
+function teardown () {
+  test('Delete temporary test repo', function (t) {
+    octo.repos(testUser, tempRepoName).remove(t.end)
+  })
 }
 
 // This is necessary because sometimes the github API cannot keep up
 // and we get errors
-function pauseAndEnd(t) {
-    return function(err) {
-        setTimeout(t.end.bind(null, err), 500);
-    };
+function pauseAndEnd (t) {
+  return function (err) {
+    setTimeout(t.end.bind(null, err), 500)
+  }
 }
 
-setup();
+setup()
 
-test('Creates new file', function(t) {
-    fs.writeFile('test.txt', 'hello world', pauseAndEnd(t));
-});
-
-test('Updates an existing file', function(t) {
-    fs.writeFile('test.txt', 'hello planet', pauseAndEnd(t));
-});
-
-test('Creates large file', function(t) {
-    fs.writeFile('test.bin', new Buffer(1100000), pauseAndEnd(t));
-});
-
-test('Updates large file', function(t) {
-    fs.writeFile('test.bin', new Buffer(1100002), pauseAndEnd(t));
-});
-
-test('Reads text file', function(t) {
-    fs.readFile('test.txt', { encoding: 'utf8' }, function(err, data) {
-        t.error(err);
-        t.equal(data, 'hello planet');
-        t.end();
-    });
-});
-
-test('Error reading non-existent file', function(t) {
-    fs.readFile('dontexist.txt', { encoding: 'utf8' }, function(err, data) {
-        t.ok(err, 'Should throw error');
-        t.equal(err.message, 'File not found')
-        t.end();
-    });
-});
-
-test('Error reading file from invalid repo', function(t) {
-    var repo = octo.repos(testUser, tempRepoName + '123');
-    var fs = hubfs(repo);
-
-    fs.readFile('test.txt', { encoding: 'utf8' }, function(err, data) {
-        t.ok(err, 'Should throw error');
-        t.equal(err.message, 'File not found')
-        t.end();
-    });
-});
-
-test('Reads binary file', function(t) {
-    fs.readFile('test.txt', function(err, data) {
-        t.error(err);
-        t.ok(bufferEqual(data, new Buffer('hello planet')), 'Buffer contents match');
-        t.end();
-    });
-});
-
-test('Reads large file', function(t) {
-    fs.readFile('test.bin', t.end);
-});
-
-test('Returns error trying to write to invalid repo', function(t) {
-    var repo = octo.repos(testUser, tempRepoName + '123');
-    var fs = hubfs(repo);
-    fs.writeFile('test.txt', 'hello planet', function(err) {
-        t.ok(err, 'should throw error')
-        t.equal(err.message, 'Invalid repo')
-        t.end()
-    });
+test('Creates new file', function (t) {
+  fs.writeFile('test.txt', 'hello world', pauseAndEnd(t))
 })
 
-teardown();
+test('Updates an existing file', function (t) {
+  fs.writeFile('test.txt', 'hello planet', pauseAndEnd(t))
+})
+
+test('Creates large file', function (t) {
+  fs.writeFile('test.bin', new Buffer(1100000), pauseAndEnd(t))
+})
+
+test('Updates large file', function (t) {
+  fs.writeFile('test.bin', new Buffer(1100002), pauseAndEnd(t))
+})
+
+test('Reads text file', function (t) {
+  fs.readFile('test.txt', { encoding: 'utf8' }, function (err, data) {
+    t.error(err)
+    t.equal(data, 'hello planet')
+    t.end()
+  })
+})
+
+test('Error reading non-existent file', function (t) {
+  fs.readFile('dontexist.txt', { encoding: 'utf8' }, function (err, data) {
+    t.ok(err, 'Should throw error')
+    t.equal(err.message, 'File not found')
+    t.end()
+  })
+})
+
+test('Error reading file from invalid repo', function (t) {
+  var repo = octo.repos(testUser, tempRepoName + '123')
+  var fs = hubfs(repo)
+
+  fs.readFile('test.txt', { encoding: 'utf8' }, function (err, data) {
+    t.ok(err, 'Should throw error')
+    t.equal(err.message, 'File not found')
+    t.end()
+  })
+})
+
+test('Reads binary file', function (t) {
+  fs.readFile('test.txt', function (err, data) {
+    t.error(err)
+    t.ok(bufferEqual(data, new Buffer('hello planet')), 'Buffer contents match')
+    t.end()
+  })
+})
+
+test('Reads large file', function (t) {
+  fs.readFile('test.bin', t.end)
+})
+
+test('Returns error trying to write to invalid repo', function (t) {
+  var repo = octo.repos(testUser, tempRepoName + '123')
+  var fs = hubfs(repo)
+  fs.writeFile('test.txt', 'hello planet', function (err) {
+    t.ok(err, 'should throw error')
+    t.equal(err.message, 'Invalid repo')
+    t.end()
+  })
+})
+
+teardown()
